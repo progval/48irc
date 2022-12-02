72fd4c9365d5e41c80afa598127e22850b1e3d93
Valentin Lorentz <progval+git@progval.net>
2022-12-02 14:24:05 +0100

    remove attempt at using readline

--- irc48/ui.py
+++ irc48/ui.py
@@ -38,48 +38,13 @@ class UI:
         self._display_queue: queue.Queue[BufferMessage] = queue.Queue()
 
     def start(self) -> None:
-        histfile = os.path.join(os.path.expanduser("~"), ".irc48_history")
-        try:
-            readline.read_history_file(histfile)
-            # default history len is -1 (infinite), which may grow unruly
-            readline.set_history_length(1000)
-        except FileNotFoundError:
-            pass
-
-        atexit.register(readline.write_history_file, histfile)
-
-        # tty.setcbreak(sys.stdin.fileno())
+        pass
 
     def loop_input(self) -> None:
-        fd = sys.stdin.fileno()
-
-        old_settings = termios.tcgetattr(fd)
-        # stdin_wrapper = io.TextIOWrapper(sys.stdin.buffer)
-        try:
-            # tty.setraw(fd)
-            buf = ""
-            while not self._state.shut_down:
-                if select.select([sys.stdin], [], [], 0.1)[0]:
-                    line = sys.stdin.readline().rstrip("\n")
-                    self._state.on_user_input(line)
-                # self._loop_input_one(stdin_wrapper)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-
-    def _loop_input_one(self, stdin_wrapper) -> None:
-        assert False
-        if select.select([sys.stdin.buffer], [], [], 0.1)[0]:
-            text = stdin_wrapper.read(1)
-            readline.insert_text(text)
-            buf = readline.get_line_buffer()
-            print(repr(readline.get_line_buffer()))
-            if "\n" in buf:
-                print("newline")
-                (command, *new_buf) = buf.split("\n", 1)
-                readline.insert_text("\x08" * len(buf))  # WTF; how else to clear it?
-                readline.insert_text(new_buf[0])
-                print(repr(command), repr(new_buf))
-            readline.redisplay()
+        while not self._state.shut_down:
+            if select.select([sys.stdin], [], [], 0.1)[0]:
+                line = sys.stdin.readline().rstrip("\n")
+                self._state.on_user_input(line)
 
     def loop_display(self) -> None:
         while not self._state.shut_down:
